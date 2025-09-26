22b290fe9d855fedea4d54e38f3990f7a6e1b0a5
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-09-26 23:17:12 +0530

    QuestionService Implementation

--- QuestionService.java
+++ QuestionService.java
@@ -2,4 +2,15 @@ class QuestionService {
 
     // Class implementation goes here
     Question question[] = new Question[5];
+
+    public QuestionService() {
+        
+    }
+
+
+    public void display() {
+       for(Question q : question){
+             System.out.println(q);
+       }
+    }
 }
