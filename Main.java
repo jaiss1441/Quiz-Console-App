e1501d37a79d0d478a417d44f80181398f8212a7
jaiss1441 <jaiswalvikas7276@gmail.com>
2025-09-27 01:23:22 +0530

    Project Completed

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         
         QuestionService qs = new QuestionService();
-        qs.display();
+        qs.playQuiz();
+        qs.displayResult();
         //System.out.println(qs.question.length);
     }
 }
--- QuestionService.java
+++ QuestionService.java
@@ -1,20 +1,50 @@
+import java.util.Scanner;
+
 class QuestionService {
 
     // Class implementation goes here
     Question question[] = new Question[5];
+    String userAnswers[] = new String[5];
 
     public QuestionService() {
         question[0] = new Question(1,"Whats is Java ?","Programming Language","Coffee","Both","None","A");
-        question[1] = new Question(2,"What is JVM ?","Java Virtual Machine","Java Very Much","Just Virtual Machine","Java Version Manager","Java Virtual Machine");
-        question[2] = new Question(3,"What is JDK ?","Java Development Kit","Java Design Kit","Just Development Kit","Java Deployment Kit","Java Development Kit");
-        question[3] = new Question(4,"What is JRE ?","Java Runtime Environment","Java Real Environment","Just Runtime Environment","Java Remote Environment","Java Runtime Environment");
-        question[4] = new Question(5,"What is an Object in OOP ?","Instance of a class","A function","A variable","A data type","Instance of a class");
+        question[1] = new Question(2,"What is JVM ?","Java Virtual Machine","Java Very Much","Just Virtual Machine","Java Version Manager","A");
+        question[2] = new Question(3,"What is JDK ?","Java Development Kit","Java Design Kit","Just Development Kit","Java Deployment Kit","A");
+        question[3] = new Question(4,"What is JRE ?","Java Real Environment","Java Runtime Environment","Just Runtime Environment","Java Remote Environment","B");
+        question[4] = new Question(5,"What is an Object in OOP ?","Instance of a class","A function","A variable","A data type","A");
     }
 
 
-    public void display() {
+    public void playQuiz() {
+        int i = 0;
        for(Question q : question){
-             System.out.println(q);
+             System.out.println("Question No :" + q.getId());
+                System.out.println(q.getQuestion());
+                System.out.println(q.getOption1());
+                System.out.println(q.getOption2()); 
+                System.out.println(q.getOption3());
+                System.out.println(q.getOption4());
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your answer : ");
+            userAnswers[i] = sc.nextLine();
+            i++;
        }
+       for(String ans : userAnswers){
+        System.out.println(ans);
+       }
+    }
+
+    public void displayResult(){
+        int score = 0;
+        for(int i = 0; i < question.length; i++){
+           Question que = question[i];
+           String userAns = userAnswers[i];
+           String correctAns = que.getAnswer();
+           if(userAns.equals(correctAns)){
+            score++;
+           }
+        }
+        System.out.println("Your score is: " + score);
     }
 }
